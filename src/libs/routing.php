5a727b34fde17e59a42d45c396450f16daf54da5
Gabriel-V-Maia <gabriel_v_maia@estudante.sesisenai.org.br>
2026-02-25 22:36:21 -0300

    fix: fix root route name

--- src/libs/routing.php
+++ src/libs/routing.php
@@ -1,5 +1,18 @@
 <?php
 
+class routeExpection extends \Exception
+{
+    const DEFAULT_MESSAGE = "Something went wrong when creating a route!";
+    
+    public function __construct($message = self::DEFAULT_MESSAGE)
+    {
+        if (!empty($message))
+            $message = DEFAULT_MESSAGE . $message; 
+        
+        parent::__construct($message);
+    }
+}
+
 class Router
 {
    protected array $routes = [];
@@ -34,15 +47,24 @@ class Router
 
 class Route
 {
+    private const VALID_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'];
+
     public string $routeName;
     public string $method;
     public $callback;
 
-    public function __construct(string $routeName, $method,callable $callback)
+    public function __construct(string $routeName, string $method, callable $callback)
     {
+        if (empty($routeName))
+            throw new RouteException("Route name can't be empty.");
+
+        $method = strtoupper($method);
+        if (!in_array($method, self::VALID_METHODS))
+            throw new RouteException("Invalid HTTP method '$method' for route '$routeName'.");
+
         $this->routeName = $routeName;
-        $this->callback = $callback;
         $this->method = $method;
+        $this->callback = $callback;
     }
 
     public function run()
